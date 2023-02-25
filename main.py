d8e27e2108db546032d0ba8fd2780c88cc4896e2
FacundoSalvi <118320324+FacundoSalvi@users.noreply.github.com>
2023-02-25 15:14:38 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import operaciones
+def main():
+    print(operaciones.suma(2,7))
+    print(operaciones.resta(5,4))
+    print(operaciones.multip(3,4))
+    print(operaciones.divis(8,2))
+
+if __name__ == '__main__':
+    main()
+
---
+++ operaciones.py
@@ -0,0 +1,12 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multip(a, b):
+    return  a * b
+
+
+def divis(a, b):
+    return  a / b
